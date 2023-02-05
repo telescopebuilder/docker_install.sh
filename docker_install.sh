bdbf4f31c9fe9b4a29c298c1c8d0b97036125b21
Andy <100099305+telescopebuilder@users.noreply.github.com>
2023-02-04 20:16:54 -0600

    Add files via upload

---
+++ docker_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install Docker Engine
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
+# Add docker user to sudo group
+sudo usermod -aG sudo docker
+
+# Start and enable Docker Engine
+sudo systemctl start docker
+sudo systemctl enable docker
